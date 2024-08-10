fc77a6ab3ad43bd3452fd906f02d18eeddf3786b
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2024-08-10 13:54:22 +0100

    feat: add dataset_viz

--- nlpcw/__version__.py
+++ nlpcw/__version__.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev1+g8025a81.d20240809'
-__version_tuple__ = version_tuple = (0, 1, 'dev1', 'g8025a81.d20240809')
+__version__ = version = '0.1.dev6+g3f73744.d20240810'
+__version_tuple__ = version_tuple = (0, 1, 'dev6', 'g3f73744.d20240810')
