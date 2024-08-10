7a2fffd16e81e5d1fd76acf3bc5dd0a934bacc1a
Rohit Krishnan <rohit.k.kesavan@gmail.com>
2024-08-10 16:08:20 +0100

    feat: remove naming, back to transformers defaults

--- nlpcw/utils.py
+++ nlpcw/utils.py
@@ -60,37 +60,14 @@ def get_dataset():
     return dataset, id2label, label2id, num_labels
 
 
-def get_model_path(
-    exp_or_model_name: str = "romainlhardy/roberta-large-finetuned-ner",
-    experiments_dir: Path = Path("experiments/")
-):
-    model_path = experiments_dir / exp_or_model_name
-    new_experiment = False
-    if not os.path.exists(model_path):
-        experiment_name = generate_random_name()
-        model_path = experiments_dir / experiment_name
-        while model_path.exists():
-            experiment_name = generate_random_name()
-            model_path = experiments_dir / experiment_name
-        
-        new_experiment = True
-    
-    return model_path, new_experiment
-
 def load_tokenizer(
     exp_or_model_name: str = "romainlhardy/roberta-large-finetuned-ner",
-    model_path: str | None = None,
 ):
     tokenizer = AutoTokenizer.from_pretrained(
         exp_or_model_name,
         add_prefix_space=True,
         clean_up_tokenization_spaces=True,
     )
-    
-    if model_path:
-        model_path.mkdir(parents=True)
-        tokenizer.save_pretrained(model_path)
-    
     return tokenizer
     
 def load_model(
@@ -98,7 +75,6 @@ def load_model(
     num_labels: int = 9,
     id2label: dict[int, str] = {},
     label2id: dict[str, int] = {},
-    model_path: str | None = None,
 ):
     log.info(f"Loading pretrained model {exp_or_model_name}")
 
@@ -115,11 +91,6 @@ def load_model(
         ignore_mismatched_sizes=True,
     )
     
-    if model_path:
-        model_path.mkdir(parents=True)
-        model.save_pretrained(model_path)
-        config_model.save_pretrained(model_path)
-
     return model, config_model
 
 
@@ -198,61 +169,3 @@ def tokenize_dataset(
         },
         desc="Tokenizing dataset",  # type: ignore
     )
-
-
-adjectives = [
-    "Agile",
-    "Brave",
-    "Clever",
-    "Dynamic",
-    "Energetic",
-    "Fearless",
-    "Gentle",
-    "Humble",
-    "Innovative",
-    "Jolly",
-    "Keen",
-    "Lively",
-    "Mighty",
-    "Nimble",
-    "Optimistic",
-    "Quick",
-    "Resilient",
-    "Steady",
-    "Tenacious",
-    "Vigorous",
-]
-
-nouns = [
-    "Explorer",
-    "Warrior",
-    "Inventor",
-    "Architect",
-    "Strategist",
-    "Scholar",
-    "Navigator",
-    "Guardian",
-    "Engineer",
-    "Artist",
-    "Pioneer",
-    "Seeker",
-    "Visionary",
-    "Mentor",
-    "Alchemist",
-    "Builder",
-    "Designer",
-    "Tactician",
-    "Sculptor",
-    "Maestro",
-]
-
-
-def generate_random_name():
-    r = random.Random()
-    adj = r.choice(adjectives)
-    n = r.choice(nouns)
-    x = r.choices(
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321", k=8
-    )
-
-    return f"{adj.lower()}-{n.lower()}-{''.join(x)}"
